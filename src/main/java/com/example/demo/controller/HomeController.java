9c79b7038b189b1bfaead98b110453f2bb04b4a8
LucasMelo59 <lucas-melo59@hotmail.com>
2022-07-12 12:01:24 -0300

    thymeleaf

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,25 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    @GetMapping("/")
     public String home  (Model model) {
-        Pedido pedido = new Pedido();
-        pedido.setNome("Televisao");
-        pedido.setValor(2222.76);
-        pedido.setUrlImagem("https://m.media-amazon.com/images/I/619vCl4hH3L._AC_SX466_.jpg");
-        pedido.setUrlProduto("https://www.amazon.com.br/PHILCO-Fast-Smart-PTV32G70RCH-Roku/dp/B09PNQZ8VH/ref=asc_df_B09PNQZ8VH/?tag=googleshopp00-20&linkCode=df0&hvadid=379725941484&hvpos=&hvnetw=g&hvrand=2127292191696726039&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001538&hvtargid=pla-1641599446695&psc=1");
-        pedido.setDescricao("qualquer coisa");
+        Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
 
-       List<Pedido> pedidos =  Arrays.asList(pedido);
+        List<Pedido> pedidos = query.getResultList();
+        model.addAttribute("pedidos", pedidos);
 
-    model.addAttribute("pedidos", pedidos);
-
-    return "home";
+        return "home";
     }
 }
--- src/main/java/com/example/demo/model/Pedido.java
+++ src/main/java/com/example/demo/model/Pedido.java
@@ -3,11 +3,19 @@ package com.example.demo.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 @Getter
 @Setter
+@Entity
 public class Pedido {
 
+
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private Double valor;
     private Date data;
